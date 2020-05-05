e18b57b253799e384ad7e57361e77b535b6582a2
Marco Vanotti <mvanott@dc.uba.ar>
2020-05-05 00:44:18 -0300

    [webserver] add json output format.

--- webserver/main.go
+++ webserver/main.go
@@ -2,12 +2,12 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/mvanotti/mtgassistant/carddb"
 	"github.com/mvanotti/mtgassistant/collectionfinder"
@@ -16,29 +16,45 @@ import (
 var (
 	mtgDataPath = flag.String("mtg_data", `C:\Program Files (x86)\Wizards of the Coast\MTGA\MTGA_Data\Downloads\Data`, "Path to the Downloads\\Data folder inside the MTG Arena Install Directory")
 	landingpage = flag.String("landing", "boostertracking.html", "Path to the landing page.")
+	jsonFormat  = flag.Bool("json", true, "Whether or not to output booster info in JSON format.")
 )
 
 const maxMtgaLogsSize int64 = 100 << 20 // 20 MiB
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseMultipartForm(maxMtgaLogsSize); err != nil {
-		log.Printf("could not parse multipart form: %v", err)
-		http.Error(w, "Invalid Request", http.StatusPreconditionFailed)
+// BoosterContents represent the contents of a MTG:Arena booster pack.
+type BoosterContents struct {
+	WcCommon   int      `json:"wcc"`
+	WcUncommon int      `json:"wcu"`
+	WcRare     int      `json:"wcr"`
+	WcMythic   int      `json:"wcm"`
+	Cards      []string `json:"cards"`
+}
+
+func outputJSON(w http.ResponseWriter, boosterData []collectionfinder.BoosterContents) {
+	var boosters []BoosterContents
+
+	w.Header().Add("Content-Type", "application/json")
+
+	for _, booster := range boosterData {
+		var contents BoosterContents
+		contents.WcCommon = booster.CommonWildcards
+		contents.WcUncommon = booster.UncommonWildcards
+		contents.WcRare = booster.RareWildcards
+		contents.WcMythic = booster.MythicWildcards
+		for _, id := range booster.CardIds {
+			card := db.GetCardByID(id)
+			c := fmt.Sprintf("%d %s (%s) %s", 1, card.Name, card.Set, card.CollectorNumber)
+			contents.Cards = append(contents.Cards, c)
+		}
+		boosters = append(boosters, contents)
 	}
 
-	file, _, err := r.FormFile("mtgalogs")
-	if err != nil {
-		log.Printf("couldnt get uploaded file: %v", err)
-		http.Error(w, "Could not retrieve mtg logs file", http.StatusPreconditionFailed)
-		return
-	}
-	defer file.Close()
-	boosterData, err := collectionfinder.FindBoosters(file)
-	if err != nil {
-		log.Fatalf("failed to parse mtga logs: %v", err)
-	}
+	enc := json.NewEncoder(w)
+	enc.Encode(boosters)
+}
 
-	fmt.Fprintf(w, "Iterating over %d boosters\n", len(boosterData))
+func outputPlain(w http.ResponseWriter, boosterData []collectionfinder.BoosterContents) {
+	w.Header().Add("Content-Type", "text/plain")
 
 	for i, booster := range boosterData {
 		fmt.Fprintf(w, "Booster #%d\n", i)
@@ -53,18 +69,36 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func boosterTracker(w http.ResponseWriter, r *http.Request) {
-	f, err := os.OpenFile(*landingpage, os.O_RDONLY, 0)
-	if err != nil {
-		log.Printf("failed to open landing page: %v", err)
-		http.Error(w, "Could not open landing page", http.StatusInternalServerError)
-		return
+func uploadHandler(dc carddb.CardDB, jsonFormat bool) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := r.ParseMultipartForm(maxMtgaLogsSize); err != nil {
+			log.Printf("could not parse multipart form: %v", err)
+			http.Error(w, "Invalid Request", http.StatusPreconditionFailed)
+		}
+
+		file, _, err := r.FormFile("mtgalogs")
+		if err != nil {
+			log.Printf("couldnt get uploaded file: %v", err)
+			http.Error(w, "Could not retrieve mtg logs file", http.StatusPreconditionFailed)
+			return
+		}
+		defer file.Close()
+		boosterData, err := collectionfinder.FindBoosters(file)
+		if err != nil {
+			log.Fatalf("failed to parse mtga logs: %v", err)
+		}
+		if jsonFormat {
+			outputJSON(w, boosterData)
+		} else {
+			outputPlain(w, boosterData)
+		}
 	}
-	if _, err := io.Copy(w, f); err != nil {
-		log.Printf("failed to copy landing page: %v", err)
-		http.Error(w, "Could not copy landing page", http.StatusInternalServerError)
+}
+
+func boosterTracker(landingpagedata []byte) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write(landingpagedata)
 	}
-	f.Close()
 }
 
 var db carddb.CardDB
@@ -74,13 +108,18 @@ func main() {
 
 	log.Println("Parsing MTG Data Files...")
 
-	var err error
+	landingpagedata, err := ioutil.ReadFile(*landingpage)
+	if err != nil {
+		log.Fatalf("failed to parse landing page file: %v", err)
+	}
+
 	db, err = carddb.CreateLibrary(*mtgDataPath)
 	if err != nil {
 		log.Fatalf("createLibrary failed: %v", err)
 	}
+
 	log.Println("Starting Server")
-	http.HandleFunc("/upload", uploadHandler)
-	http.HandleFunc("/boostertracking", boosterTracker)
+	http.HandleFunc("/upload", uploadHandler(db, *jsonFormat))
+	http.HandleFunc("/boostertracking", boosterTracker(landingpagedata))
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
